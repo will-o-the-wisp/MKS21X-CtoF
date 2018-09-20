808fba33cf7092cf4e918711ceae0dd73d7f8b43
will-o-the-wisp <kli18@stuy.edu>
2018-09-19 21:14:21 -0400

    Created file with empty main and converting functions

---
+++ CtoFTester.java
@@ -0,0 +1,11 @@
+public class CtoFTester{
+  public static void main(float x, float y) {
+    return;
+  }
+  public static float celsiusToFahrenheit(float x){
+    return 1;
+  }
+  public static float fahrenheitToCelsius(float x){
+    return 1;
+  }
+}
