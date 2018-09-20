9e440843ecbda9e34cfc5a42febb5d2f43ac4f46
will-o-the-wisp <kli18@stuy.edu>
2018-09-19 21:22:01 -0400

    Created converter functions and fully debugged

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,11 +1,12 @@
 public class CtoFTester{
-  public static void main(float x, float y) {
-    return;
+  public static void main(String[] args) {
+    System.out.println(celsiusToFahrenheit(100));
+    System.out.println(fahrenheitToCelsius(212));
   }
-  public static float celsiusToFahrenheit(float x){
-    return 1;
+  public static double celsiusToFahrenheit(double x){
+    return 1.8*x+32;
   }
-  public static float fahrenheitToCelsius(float x){
-    return 1;
+  public static double fahrenheitToCelsius(double x){
+    return (x-32)/1.8;
   }
 }
